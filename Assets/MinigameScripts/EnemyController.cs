1169b5ec66ee4f59027ff6cafe247d1ee2cb95e7
ahwlsdn123 <ahwlsdn123@naver.com>
2024-11-07 11:47:59 +0900

    Add Minigame Scripts

---
+++ Assets/MinigameScripts/EnemyController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 100f;
+    public RectTransform minigamePanel; // MinigamePanel의 RectTransform을 직접 연결
+
+    void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        // Y 위치를 검사하여 파괴
+        if (transform.localPosition.y < -minigamePanel.rect.height / 2 + 13f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
